4eb3ec52c155758eab2f8863926842cbf6ee982a
Matthew Valencia <MatthewLuisValencia@gmail.com>
2019-02-28 12:08:17 -0600

    More In-class code examples

---
+++ adoNetConnected/adoNetConnected/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace adoNetConnected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //bad idea to have username + password in code, insecure
+            var conString = "Server = tcp:matthew1995sql.database.windows.net,1433; Initial Catalog = chinook; Persist Security Info = False; User ID = { your_username }; Password ={ your_password}; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+
+            SqlConnection connection = new SqlConnection();
+        }
+    }
+}
